21ad032781666b54c85e1c88cc0c38888addcb69
Winds-b <1577910536@qq.com>
2016-09-07 15:39:25 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a1/sqlite/MainActivity.java
@@ -0,0 +1,436 @@
+package com.example.a1.sqlite;
+
+import android.app.AlertDialog;
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.SimpleAdapter;
+import android.widget.TableLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class MainActivity extends AppCompatActivity {
+    WordsDBHelper mDbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //新增单词
+                InsertDialog();
+            }
+        });
+
+        //为ListView注册上下文菜单
+        ListView list = (ListView) findViewById(R.id.lstWords);
+        registerForContextMenu(list);
+
+
+        //创建SQLiteOpenHelper对象，注意第一次运行时，此时数据库并没有被创建
+        mDbHelper = new WordsDBHelper(this);
+
+        //在列表显示全部单词
+        ArrayList<Map<String, String>> items = getAll();
+        setWordsListView(items);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mDbHelper.close();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_search:
+                //查找
+                SearchDialog();
+                return true;
+            case R.id.action_insert:
+                //新增单词
+                InsertDialog();
+                return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        getMenuInflater().inflate(R.menu.contextmenu_wordslistview, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        TextView textId = null;
+        TextView textWord = null;
+        TextView textMeaning = null;
+        TextView textSample = null;
+
+        AdapterView.AdapterContextMenuInfo info = null;
+        View itemView = null;
+
+        switch (item.getItemId()) {
+            case R.id.action_delete:
+                //删除单词
+                info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+                itemView = info.targetView;
+                textId = (TextView) itemView.findViewById(R.id.textId);
+                if (textId != null) {
+                    String strId = textId.getText().toString();
+                    DeleteDialog(strId);
+                }
+                break;
+            case R.id.action_update:
+                //修改单词
+                info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+                itemView = info.targetView;
+                textId = (TextView) itemView.findViewById(R.id.textId);
+                textWord = (TextView) itemView.findViewById(R.id.textViewWord);
+                textMeaning = (TextView) itemView.findViewById(R.id.textViewMeaning);
+                textSample = (TextView) itemView.findViewById(R.id.textViewSample);
+                if (textId != null && textWord != null && textMeaning != null && textSample != null) {
+                    String strId = textId.getText().toString();
+                    String strWord = textWord.getText().toString();
+                    String strMeaning = textMeaning.getText().toString();
+                    String strSample = textSample.getText().toString();
+                    UpdateDialog(strId, strWord, strMeaning, strSample);
+                }
+                break;
+        }
+        return true;
+    }
+
+    //设置适配器，在列表中显示单词
+    private void setWordsListView(ArrayList<Map<String, String>> items) {
+        SimpleAdapter adapter = new SimpleAdapter(this, items, R.layout.item,
+                new String[]{Words.Word._ID, Words.Word.COLUMN_NAME_WORD, Words.Word.COLUMN_NAME_MEANING, Words.Word.COLUMN_NAME_SAMPLE},
+                new int[]{R.id.textId, R.id.textViewWord, R.id.textViewMeaning, R.id.textViewSample});
+
+        ListView list = (ListView) findViewById(R.id.lstWords);
+
+        list.setAdapter(adapter);
+    }
+
+
+    private ArrayList<Map<String, String>> getAll() {
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+
+        String[] projection = {
+                Words.Word._ID,
+                Words.Word.COLUMN_NAME_WORD,
+                Words.Word.COLUMN_NAME_MEANING,
+                Words.Word.COLUMN_NAME_SAMPLE
+        };
+
+        //排序
+        String sortOrder =
+                Words.Word.COLUMN_NAME_WORD + " DESC";
+
+
+        Cursor c = db.query(
+                Words.Word.TABLE_NAME,
+                projection,
+                null,
+                null,
+                null,
+                null,
+                sortOrder
+        );
+
+        return ConvertCursor2List(c);
+    }
+
+
+    private ArrayList<Map<String, String>> ConvertCursor2List(Cursor cursor) {
+        ArrayList<Map<String, String>> result = new ArrayList<>();
+        while (cursor.moveToNext()) {
+            Map<String, String> map = new HashMap<>();
+            map.put(Words.Word._ID, String.valueOf(cursor.getInt(0)));
+            map.put(Words.Word.COLUMN_NAME_WORD, cursor.getString(1));
+            map.put(Words.Word.COLUMN_NAME_MEANING, cursor.getString(2));
+            map.put(Words.Word.COLUMN_NAME_SAMPLE, cursor.getString(3));
+            result.add(map);
+        }
+        return result;
+    }
+
+    //使用Sql语句插入单词
+    private void InsertUserSql(String strWord, String strMeaning, String strSample) {
+        String sql = "insert into  words(word,meaning,sample) values(?,?,?)";
+
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+        db.execSQL(sql, new String[]{strWord, strMeaning, strSample});
+    }
+
+    //使用insert方法增加单词
+    private void Insert(String strWord, String strMeaning, String strSample) {
+
+
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+
+        ContentValues values = new ContentValues();
+        values.put(Words.Word.COLUMN_NAME_WORD, strWord);
+        values.put(Words.Word.COLUMN_NAME_MEANING, strMeaning);
+        values.put(Words.Word.COLUMN_NAME_SAMPLE, strSample);
+
+        long newRowId;
+        newRowId = db.insert(
+                Words.Word.TABLE_NAME,
+                null,
+                values);
+    }
+
+
+    //新增对话框
+    private void InsertDialog() {
+        final RelativeLayout relativeLayout = (RelativeLayout) getLayoutInflater().inflate(R.layout.insert, null);
+        new AlertDialog.Builder(this)
+                .setTitle("新增单词")//标题
+                .setView(relativeLayout)//设置视图
+                //确定按钮及其动作
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String strWord = ((EditText) relativeLayout.findViewById(R.id.txtWord)).getText().toString();
+                        String strMeaning = ((EditText) relativeLayout.findViewById(R.id.txtMeaning)).getText().toString();
+                        String strSample = ((EditText) relativeLayout.findViewById(R.id.txtSample)).getText().toString();
+
+                        //既可以使用Sql语句插入，也可以使用使用insert方法插入
+                        Insert(strWord, strMeaning, strSample);
+
+                        ArrayList<Map<String, String>> items = getAll();
+                        setWordsListView(items);
+
+                    }
+                })
+                //取消按钮及其动作
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                })
+                .create()//创建对话框
+                .show();//显示对话框
+
+
+    }
+
+    //使用Sql语句删除单词
+    private void DeleteUseSql(String strId) {
+        String sql = "delete from words where _id='" + strId + "'";
+
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+        db.execSQL(sql);
+    }
+
+    //删除单词
+    private void Delete(String strId) {
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+        // 定义where子句
+        String selection = Words.Word._ID + " = ?";
+
+        // 指定占位符对应的实际参数
+        String[] selectionArgs = {strId};
+
+        db.delete(Words.Word.TABLE_NAME, selection, selectionArgs);
+    }
+
+
+    //删除对话框
+    private void DeleteDialog(final String strId) {
+        new AlertDialog.Builder(this).setTitle("删除单词").setMessage("是否真的删除单词?").setPositiveButton("确定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //既可以使用Sql语句删除，也可以使用使用delete方法删除
+                DeleteUseSql(strId);
+                //Delete(strId);
+                setWordsListView(getAll());
+            }
+        }).setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        }).create().show();
+    }
+
+    //使用Sql语句更新单词
+    private void UpdateUseSql(String strId, String strWord, String strMeaning, String strSample) {
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+        String sql = "update words set word=?,meaning=?,sample=? where _id=?";
+        db.execSQL(sql, new String[]{strWord, strMeaning, strSample, strId});
+    }
+
+    //使用方法更新
+    private void Update(String strId, String strWord, String strMeaning, String strSample) {
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+
+        Log.v("My", strId);
+        Log.v("My", strWord);
+        Log.v("My", strMeaning);
+        Log.v("My", strSample);
+
+        ContentValues values = new ContentValues();
+        values.put(Words.Word.COLUMN_NAME_WORD, strWord);
+        values.put(Words.Word.COLUMN_NAME_MEANING, strMeaning);
+        values.put(Words.Word.COLUMN_NAME_SAMPLE, strSample);
+
+        String selection = Words.Word._ID + " = ?";
+        String[] selectionArgs = {strId};
+
+        int count = db.update(
+                Words.Word.TABLE_NAME,
+                values,
+                selection,
+                selectionArgs);
+    }
+
+    //修改对话框
+    private void UpdateDialog(final String strId, final String strWord, final String strMeaning, final String strSample) {
+        final RelativeLayout relativeLayout = (RelativeLayout) getLayoutInflater().inflate(R.layout.insert, null);
+        ((EditText) relativeLayout.findViewById(R.id.txtWord)).setText(strWord);
+        ((EditText) relativeLayout.findViewById(R.id.txtMeaning)).setText(strMeaning);
+        ((EditText) relativeLayout.findViewById(R.id.txtSample)).setText(strSample);
+        new AlertDialog.Builder(this)
+                .setTitle("修改单词")//标题
+                .setView(relativeLayout)//设置视图
+                //确定按钮及其动作
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String strNewWord = ((EditText) relativeLayout.findViewById(R.id.txtWord)).getText().toString();
+                        String strNewMeaning = ((EditText) relativeLayout.findViewById(R.id.txtMeaning)).getText().toString();
+                        String strNewSample = ((EditText) relativeLayout.findViewById(R.id.txtSample)).getText().toString();
+
+                        //既可以使用Sql语句更新，也可以使用使用update方法更新
+                        UpdateUseSql(strId, strNewWord, strNewMeaning, strNewSample);
+                        setWordsListView(getAll());
+                    }
+                })
+                //取消按钮及其动作
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                })
+                .create()//创建对话框
+                .show();//显示对话框
+
+
+    }
+
+
+    //使用query方法查找
+    private ArrayList<Map<String, String>> Search(String strWordSearch) {
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+
+        String[] projection = {
+                Words.Word._ID,
+                Words.Word.COLUMN_NAME_WORD,
+                Words.Word.COLUMN_NAME_MEANING,
+                Words.Word.COLUMN_NAME_SAMPLE
+        };
+
+        String sortOrder =
+                Words.Word.COLUMN_NAME_WORD + " DESC";
+
+        String selection = Words.Word.COLUMN_NAME_WORD + " LIKE ?";
+        String[] selectionArgs = {"%" + strWordSearch + "%"};
+
+        Cursor c = db.query(
+                Words.Word.TABLE_NAME,
+                projection,
+                selection,
+                selectionArgs,
+                null,
+                null,
+                sortOrder
+        );
+
+        return ConvertCursor2List(c);
+    }
+
+    //查找对话框
+    private void SearchDialog() {
+        final TableLayout tableLayout = (TableLayout) getLayoutInflater().inflate(R.layout.searchterm, null);
+        new AlertDialog.Builder(this)
+                .setTitle("查找单词")//标题
+                .setView(tableLayout)//设置视图
+                //确定按钮及其动作
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String txtSearchWord = ((EditText) tableLayout.findViewById(R.id.txtSearchWord)).getText().toString();
+
+                        ArrayList<Map<String, String>> items = Search(txtSearchWord);
+                        //既可以使用Sql语句查询，也可以使用方法查询
+
+                        if (items.size() > 0) {
+                            Bundle bundle = new Bundle();
+                            bundle.putSerializable("result", items);
+                            Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+                            intent.putExtras(bundle);
+                            startActivity(intent);
+                        } else
+                            Toast.makeText(MainActivity.this, "没有找到", Toast.LENGTH_LONG).show();
+
+
+                    }
+                })
+                //取消按钮及其动作
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                })
+                .create()//创建对话框
+                .show();//显示对话框
+
+    }
+}
+
---
+++ app/src/main/java/com/example/a1/sqlite/Words.java
@@ -0,0 +1,17 @@
+package com.example.a1.sqlite;
+
+import android.provider.BaseColumns;
+/**
+ * Created by 1 on 2016/9/7.
+ */
+public class Words {
+    public Words() {
+    }
+
+    public static abstract class Word implements BaseColumns {
+        public static final String TABLE_NAME = "words";
+        public static final String COLUMN_NAME_WORD = "word";//列：单词
+        public static final String COLUMN_NAME_MEANING = "meaning";//列：单词含义
+        public static final String COLUMN_NAME_SAMPLE = "sample";//单词示例
+    }
+}
